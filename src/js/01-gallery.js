20e03ee46076df99da024d611b14862164d58237
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-06-20 15:33:07 +0300

    set parcel

---
+++ src/js/01-gallery.js
@@ -0,0 +1,42 @@
+// import '../css/01-gallery.css';
+// import '../css/common.css';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
+
+// import 'lazysizes';
+// import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const pageGallery = document.querySelector('.gallery');
+const cardImage = createGallery(galleryItems);
+
+pageGallery.insertAdjacentHTML('beforeend', cardImage);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image lazyload"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+// Lightbox library
+new SimpleLightbox('.gallery a', {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
